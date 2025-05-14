27b3b1ccfcb473b53b40833b399bb81c3991ebdb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-14 10:19:02 -0400

    Update os.d

--- source/os.d
+++ source/os.d
@@ -34,3 +34,45 @@ public:
 	static void log(const c8* message, const path* hint, ELOG_LEVEL ll = ELOG_LEVEL.ELL_INFORMATION);
 	static ILogger* Logger;
 };
+
+class Timer {
+public:
+	//! returns the current time in milliseconds
+	static u32 getTime();
+
+	//! initializes the real timer
+	static void initTimer();
+
+	//! sets the current virtual (game) time
+	static void setTime(u32 time);
+
+	//! stops the virtual (game) timer
+	static void stopTimer();
+
+	//! starts the game timer
+	static void startTimer();
+
+	//! sets the speed of the virtual timer
+	static void setSpeed(f32 speed);
+
+	//! gets the speed of the virtual timer
+	static f32 getSpeed();
+
+	//! returns if the timer currently is stopped
+	static bool isStopped();
+
+	//! makes the virtual timer update the time value based on the real time
+	static void tick();
+
+	//! returns the current real time in milliseconds
+	static u32 getRealTime();
+
+private:
+	static void initVirtualTimer();
+
+	static f32 VirtualTimerSpeed;
+	static s32 VirtualTimerStopCounter;
+	static u32 StartRealTime;
+	static u32 LastVirtualTime;
+	static u32 StaticTime;
+};
