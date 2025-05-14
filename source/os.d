ca6868e512be0e931363c9ab1a00e256b3a754da
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-14 10:18:46 -0400

    Update os.d

--- source/os.d
+++ source/os.d
@@ -1,5 +1,7 @@
 module os;
 
+import i_logger;
+import irr_path;
 import irr_types;
 
 class ByteSwap {
@@ -18,7 +20,17 @@ class ByteSwap {
 
 	static c8 byteswap(c8 num) {
 		return num;
-	}	
+	}
 }
 
+class Printer {
+public:
+	// prints out a string to the console out stdout or debug log or whatever
+	static void print(const c8* message, ELOG_LEVEL ll = ELOG_LEVEL.ELL_INFORMATION);
+	static void log(const c8* message, ELOG_LEVEL ll = ELOG_LEVEL.ELL_INFORMATION);
 
+	// The string ": " is added between message and hint
+	static void log(const c8* message, const c8* hint, ELOG_LEVEL ll = ELOG_LEVEL.ELL_INFORMATION);
+	static void log(const c8* message, const path* hint, ELOG_LEVEL ll = ELOG_LEVEL.ELL_INFORMATION);
+	static ILogger* Logger;
+};
