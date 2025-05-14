991ef5e758df02d6a64363fe00d41f6494d3d113
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-14 08:16:53 -0400

    Use Ddoc

--- source/irr_types.d
+++ source/irr_types.d
@@ -1,37 +1,37 @@
 module irr_types;
 
-//! 8 bit unsigned variable.
+/// 8 bit unsigned variable.
 alias u8 = ubyte;
 
-//! 8 bit signed variable.
+/// 8 bit signed variable.
 alias s8 = byte;
 
-//! 8 bit character variable.
 /** This is a typedef for char, it ensures portability of the engine. */
+/// 8 bit character variable.
 alias c8 = char;
 
-//! 16 bit unsigned variable.
+/// 16 bit unsigned variable.
 alias u16 = ushort;
 
-//! 16 bit signed variable.
+/// 16 bit signed variable.
 alias s16 = short;
 
-//! 32 bit unsigned variable.
+/// 32 bit unsigned variable.
 alias u32 = uint;
 
-//! 32 bit signed variable.
+/// 32 bit signed variable.
 alias s32 = int;
 
-//! 64 bit unsigned variable.
+/// 64 bit unsigned variable.
 alias u64 = ulong;
 
-//! 64 bit signed variable.
+/// 64 bit signed variable.
 alias s64 = long;
 
-//! 32 bit floating point variable.
 /** This is a typedef for float, it ensures portability of the engine. */
+/// 32 bit floating point variable.
 alias f32 = float;
 
-//! 64 bit floating point variable.
 /** This is a typedef for double, it ensures portability of the engine. */
+/// 64 bit floating point variable.
 alias f64 = double;
