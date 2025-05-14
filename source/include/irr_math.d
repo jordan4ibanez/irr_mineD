046ede06172e458de92a4043dfa2138017132685
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-14 11:23:25 -0400

    Update irr_math.d

--- source/include/irr_math.d
+++ source/include/irr_math.d
@@ -1,5 +1,6 @@
 module include.irr_math;
 
+import include.irr_types;
 import std.math.constants;
 
 // Copyright (C) 2002-2012 Nikolaus Gebhardt
@@ -32,7 +33,7 @@ static immutable f64 ROUNDING_ERROR_f64 = 0.00000001;
 // #endif
 
 //! Constant for PI.
-static immutable f32 PI = PI; // <- Incredible
+// static immutable f32 PI = PI;
 
 // #ifdef PI64 // make sure we don't collide with a define
 // #undef PI64
@@ -56,7 +57,7 @@ static immutable f64 RADTODEG64 = 180.0 / PI64;
 /** Provided as it can be clearer to write radToDeg(X) than RADTODEG * X
 \param radians The radians value to convert to degrees.
 */
-  f32 radToDeg(f32 radians) 
+ pragma(inline, true) f32 radToDeg(f32 radians) 
 {
 	return RADTODEG * radians;
 }
@@ -65,7 +66,7 @@ static immutable f64 RADTODEG64 = 180.0 / PI64;
 /** Provided as it can be clearer to write radToDeg(X) than RADTODEG * X
 \param radians The radians value to convert to degrees.
 */
-f64 radToDeg(f64 radians)
+pragma(inline, true) f64 radToDeg(f64 radians)
 {
 	return RADTODEG64 * radians;
 }
@@ -74,7 +75,7 @@ f64 radToDeg(f64 radians)
 /** Provided as it can be clearer to write degToRad(X) than DEGTORAD * X
 \param degrees The degrees value to convert to radians.
 */
- f32 degToRad(f32 degrees)
+ pragma(inline, true) f32 degToRad(f32 degrees)
 {
 	return DEGTORAD * degrees;
 }
@@ -83,42 +84,42 @@ f64 radToDeg(f64 radians)
 /** Provided as it can be clearer to write degToRad(X) than DEGTORAD * X
 \param degrees The degrees value to convert to radians.
 */
- f64 degToRad(f64 degrees)
+ pragma(inline, true) f64 degToRad(f64 degrees)
 {
 	return DEGTORAD64 * degrees;
 }
 
 //! returns minimum of two values.
 // template <class T>
-  T min_(T)(const ref T a, const ref T b)const
+  pragma(inline, true) T min_(T)(const ref T a, const ref T b)const
 {
 	return a < b ? a : b;
 }
 
 //! returns minimum of three values.
 // template <class T>
-  T min_(T)(const ref T a, const ref T b, const ref T c)const
+  pragma(inline, true) T min_(T)(const ref T a, const ref T b, const ref T c)const
 {
 	return a < b ? min_(a, c) : min_(b, c);
 }
 
 //! returns maximum of two values.
 // template <class T>
-  T max_(T)(const ref T a, const ref T b)const
+  pragma(inline, true) T max_(T)(const ref T a, const ref T b)const
 {
 	return a < b ? b : a;
 }
 
 //! returns maximum of three values.
 // template <class T>
-  T max_(T)(const ref T a, const ref T b, const ref T c)const
+  pragma(inline, true) T max_(T)(const ref T a, const ref T b, const ref T c)const
 {
 	return a < b ? max_(b, c) : max_(a, c);
 }
 
 //! returns abs of two values.
 // template <class T>
- T abs_(T)(const ref T a)
+ pragma(inline, true) T abs_(T)(const ref T a)
 {
 	return abs(a);
 }
@@ -126,14 +127,14 @@ f64 radToDeg(f64 radians)
 //! returns linear interpolation of a and b with ratio t
 //! \return: a if t==0, b if t==1, and the linear interpolation else
 // template <class T>
- T lerp(const ref T a, const ref T b, const f32 t)
+ pragma(inline, true) T lerp(const ref T a, const ref T b, const f32 t)
 {
 	return (T)(a * (1.f - t)) + (b * t);
 }
 
 //! clamps a value between low and high
 // template <class T>
-  T clamp(const ref T value, const ref T low, const ref T high) const
+  pragma(inline, true) T clamp(const ref T value, const ref T low, const ref T high) const
 {
 	return min_(max_(value, low), high);
 }
@@ -142,45 +143,45 @@ f64 radToDeg(f64 radians)
 //  T roundingError();
 
 // template <>
- f32 roundingError()
+ pragma(inline, true) f32 roundingError()
 {
 	return ROUNDING_ERROR_f32;
 }
 
 // template <>
- f64 roundingError()
+ pragma(inline, true) f64 roundingError()
 {
 	return ROUNDING_ERROR_f64;
 }
 
 // template <class T>
- T relativeErrorFactor()
+ pragma(inline, true) T relativeErrorFactor()
 {
 	return 1;
 }
 
 // template <>
- f32 relativeErrorFactor()
+ pragma(inline, true) f32 relativeErrorFactor()
 {
 	return 4;
 }
 
 // template <>
- f64 relativeErrorFactor()
+ pragma(inline, true) f64 relativeErrorFactor()
 {
 	return 8;
 }
 
 //! returns if a equals b, for types without rounding errors
 // template <class T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
- bool equals(T)(const T a, const T b)
+ pragma(inline, true) bool equals(T)(const T a, const T b)
 {
 	return a == b;
 }
 
 //! returns if a equals b, taking possible rounding errors into account
 // template <class T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
- bool equals(T)(const T a, const T b, const T tolerance = roundingError())
+ pragma(inline, true) bool equals(T)(const T a, const T b, const T tolerance = roundingError())
 {
 	return abs(a - b) <= tolerance;
 }
@@ -188,7 +189,7 @@ f64 radToDeg(f64 radians)
 //! returns if a equals b, taking relative error in form of factor
 //! this particular function does not involve any division.
 // template <class T>
- bool equalsRelative(T)(const T a, const T b, const T factor = relativeErrorFactor())
+ pragma(inline, true) bool equalsRelative(T)(const T a, const T b, const T factor = relativeErrorFactor())
 {
 	// https://eagergames.wordpress.com/2017/04/01/fast-parallel-lines-and-vectors-test/
 
@@ -214,7 +215,7 @@ union FloatIntUnion32
 
 //! We compare the difference in ULP's (spacing between floating-point numbers, aka ULP=1 means there exists no float between).
 //\result true when numbers have a ULP <= maxUlpDiff AND have the same sign.
- bool equalsByUlp(f32 a, f32 b, int maxUlpDiff)
+ pragma(inline, true) bool equalsByUlp(f32 a, f32 b, int maxUlpDiff)
 {
 	// Based on the ideas and code from Bruce Dawson on
 	// http://www.altdevblogaday.com/2012/02/22/comparing-floating-point-numbers-2012-edition/
@@ -242,58 +243,58 @@ union FloatIntUnion32
 }
 
 //! returns if a equals zero, taking rounding errors into account
-bool iszero(const f64 a, const f64 tolerance = ROUNDING_ERROR_f64)
+pragma(inline, true) bool iszero(const f64 a, const f64 tolerance = ROUNDING_ERROR_f64)
 {
 	return fabs(a) <= tolerance;
 }
 
 //! returns if a equals zero, taking rounding errors into account
-bool iszero(const f32 a, const f32 tolerance = ROUNDING_ERROR_f32)
+pragma(inline, true) bool iszero(const f32 a, const f32 tolerance = ROUNDING_ERROR_f32)
 {
 	return fabsf(a) <= tolerance;
 }
 
 //! returns if a equals not zero, taking rounding errors into account
-bool isnotzero(const f32 a, const f32 tolerance = ROUNDING_ERROR_f32)
+pragma(inline, true) bool isnotzero(const f32 a, const f32 tolerance = ROUNDING_ERROR_f32)
 {
 	return fabsf(a) > tolerance;
 }
 
 //! returns if a equals zero, taking rounding errors into account
-bool iszero(const s32 a, const s32 tolerance = 0)
+pragma(inline, true) bool iszero(const s32 a, const s32 tolerance = 0)
 {
 	return (a & 0x7ffffff) <= tolerance;
 }
 
 //! returns if a equals zero, taking rounding errors into account
-bool iszero(const u32 a, const u32 tolerance = 0)
+pragma(inline, true) bool iszero(const u32 a, const u32 tolerance = 0)
 {
 	return a <= tolerance;
 }
 
 //! returns if a equals zero, taking rounding errors into account
-bool iszero(const s64 a, const s64 tolerance = 0)
+pragma(inline, true) bool iszero(const s64 a, const s64 tolerance = 0)
 {
 	return abs_(a) <= tolerance;
 }
 
-s32 s32_min(s32 a, s32 b)
+pragma(inline, true) s32 s32_min(s32 a, s32 b)
 {
 	return min_(a, b);
 }
 
-s32 s32_max(s32 a, s32 b)
+pragma(inline, true) s32 s32_max(s32 a, s32 b)
 {
 	return max_(a, b);
 }
 
-s32 s32_clamp(s32 value, s32 low, s32 high)
+pragma(inline, true) s32 s32_clamp(s32 value, s32 low, s32 high)
 {
 	return clamp(value, low, high);
 }
 
 // integer log2 of an integer. returning 0 if denormal
-s32 u32_log2(u32 input)
+pragma(inline, true) s32 u32_log2(u32 input)
 {
 	s32 ret = 0;
 	while (input > 1) {
@@ -323,11 +324,11 @@ s32 u32_log2(u32 input)
 	f32 f;
 } 
 
-s32 F32_AS_S32(f32 f) {
+pragma(inline, true) s32 F32_AS_S32(f32 f) {
     return (*(cast(s32 *)&(f)));
 }
 
-u32 F32_AS_U32(f32 f) {
+pragma(inline, true) u32 F32_AS_U32(f32 f) {
     return (*(cast(u32 *)&(f)));
 }
 
@@ -340,7 +341,7 @@ static immutable  F32_VALUE_1 = 0x3f800000;
 
 //! code is taken from IceFPU
 //! Integer representation of a floating-point value.
-u32 IR(f32 x)
+pragma(inline, true) u32 IR(f32 x)
 {
 	inttofloat tmp;
 	tmp.f = x;
@@ -348,150 +349,150 @@ u32 IR(f32 x)
 }
 
 //! Floating-point representation of an integer value.
-f32 FR(u32 x)
+pragma(inline, true) f32 FR(u32 x)
 {
 	inttofloat tmp;
 	tmp.u = x;
 	return tmp.f;
 }
-f32 FR(s32 x)
+pragma(inline, true) f32 FR(s32 x)
 {
 	inttofloat tmp;
 	tmp.s = x;
 	return tmp.f;
 }
 
-bool F32_LOWER_0(f32 n) {
+pragma(inline, true) bool F32_LOWER_0(f32 n) {
     return ((n) < 0.0f);
 }
 
-bool F32_LOWER_EQUAL_0(f32 n) {
+pragma(inline, true) bool F32_LOWER_EQUAL_0(f32 n) {
     return ((n) <= 0.0f);
 }
 
-bool F32_GREATER_0(f32 n) {
+pragma(inline, true) bool F32_GREATER_0(f32 n) {
     return ((n) > 0.0f);
 }
 
-bool F32_GREATER_EQUAL_0(f32 n){
+pragma(inline, true) bool F32_GREATER_EQUAL_0(f32 n){
  return ((n) >= 0.0f);
 }
 
-bool F32_EQUAL_1(f32 n) {
+pragma(inline, true) bool F32_EQUAL_1(f32 n) {
     return ((n) == 1.0f);
 }
 
-bool F32_EQUAL_0(f32 n) {
+pragma(inline, true) bool F32_EQUAL_0(f32 n) {
     return ((n) == 0.0f);
 }
 
-bool F32_A_GREATER_B(f32 a, f32 b) {
+pragma(inline, true) bool F32_A_GREATER_B(f32 a, f32 b) {
     return ((a) > (b));
 }
 
-#ifndef REALINLINE
-#ifdef _MSC_VER
-#define REALINLINE __forceinline
-#else
-#define REALINLINE inline
-#endif
-#endif
+// #ifndef REALINLINE
+// #ifdef _MSC_VER
+// #define REALINLINE __forceinline
+// #else
+// #define REALINLINE inline
+// #endif
+// #endif
 
 // NOTE: This is not as exact as the c99/c++11 round function, especially at high numbers starting with 8388609
 //       (only low number which seems to go wrong is 0.49999997 which is rounded to 1)
 //      Also negative 0.5 is rounded up not down unlike with the standard function (p.E. input -0.5 will be 0 and not -1)
-inline f32 round_(f32 x)
+pragma(inline, true) f32 round_(f32 x)
 {
 	return floorf(x + 0.5f);
 }
 
 // calculate: sqrt ( x )
-REALINLINE f32 squareroot(const f32 f)
+pragma(inline, true)  f32 squareroot(const f32 f)
 {
 	return sqrtf(f);
 }
 
 // calculate: sqrt ( x )
-REALINLINE f64 squareroot(const f64 f)
+pragma(inline, true)  f64 squareroot(const f64 f)
 {
 	return sqrt(f);
 }
 
 // calculate: sqrt ( x )
-REALINLINE s32 squareroot(const s32 f)
+pragma(inline, true)  s32 squareroot(const s32 f)
 {
-	return static_cast<s32>(squareroot(static_cast<f32>(f)));
+	return cast(s32)(squareroot(cast(f32)(f)));
 }
 
 // calculate: sqrt ( x )
-REALINLINE s64 squareroot(const s64 f)
+pragma(inline, true)  s64 squareroot(const s64 f)
 {
-	return static_cast<s64>(squareroot(static_cast<f64>(f)));
+	return cast(s64)(squareroot(cast(f64)(f)));
 }
 
 // calculate: 1 / sqrt ( x )
-REALINLINE f64 reciprocal_squareroot(const f64 x)
+pragma(inline, true)  f64 reciprocal_squareroot(const f64 x)
 {
 	return 1.0 / sqrt(x);
 }
 
 // calculate: 1 / sqrtf ( x )
-REALINLINE f32 reciprocal_squareroot(const f32 f)
+pragma(inline, true)  f32 reciprocal_squareroot(const f32 f)
 {
 	return 1.f / sqrtf(f);
 }
 
 // calculate: 1 / sqrtf( x )
-REALINLINE s32 reciprocal_squareroot(const s32 x)
+pragma(inline, true)  s32 reciprocal_squareroot(const s32 x)
 {
-	return static_cast<s32>(reciprocal_squareroot(static_cast<f32>(x)));
+	return cast(s32)(reciprocal_squareroot(cast(f32)(x)));
 }
 
 // calculate: 1 / x
-REALINLINE f32 reciprocal(const f32 f)
+pragma(inline, true)  f32 reciprocal(const f32 f)
 {
 	return 1.f / f;
 }
 
 // calculate: 1 / x
-REALINLINE f64 reciprocal(const f64 f)
+pragma(inline, true)  f64 reciprocal(const f64 f)
 {
 	return 1.0 / f;
 }
 
 // calculate: 1 / x, low precision allowed
-REALINLINE f32 reciprocal_approxim(const f32 f)
+pragma(inline, true)  f32 reciprocal_approxim(const f32 f)
 {
 	return 1.f / f;
 }
 
-REALINLINE s32 floor32(f32 x)
+pragma(inline, true)  s32 floor32(f32 x)
 {
-	return (s32)floorf(x);
+	return cast(s32)floorf(x);
 }
 
-REALINLINE s32 ceil32(f32 x)
+pragma(inline, true)  s32 ceil32(f32 x)
 {
-	return (s32)ceilf(x);
+	return cast(s32)ceilf(x);
 }
 
 // NOTE: Please check round_ documentation about some inaccuracies in this compared to standard library round function.
-REALINLINE s32 round32(f32 x)
+pragma(inline, true)  s32 round32(f32 x)
 {
-	return (s32)round_(x);
+	return cast(s32)round_(x);
 }
 
-inline f32 f32_max3(const f32 a, const f32 b, const f32 c)
+pragma(inline, true) f32 f32_max3(const f32 a, const f32 b, const f32 c)
 {
 	return a > b ? (a > c ? a : c) : (b > c ? b : c);
 }
 
-inline f32 f32_min3(const f32 a, const f32 b, const f32 c)
+pragma(inline, true) f32 f32_min3(const f32 a, const f32 b, const f32 c)
 {
 	return a < b ? (a < c ? a : c) : (b < c ? b : c);
 }
 
-inline f32 fract(f32 x)
+pragma(inline, true) f32 fract(f32 x)
 {
 	return x - floorf(x);
 }
