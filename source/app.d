8e5f71ff2021cb8892b038ac25123be9a1831301
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-15 18:20:36 -0400

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,14 +1,20 @@
 module app;
 
+import include.dimension_2d;
+import include.vector2d;
 import include.vector3d;
 import std.stdio;
 
 void main() {
-    vector3d!float test;
-    test.invert();
-    test += vector3d!float(1, 2, 3) + [1, 2, 3] + 5;
-    test *= [1, 2, 3];
-    test = test + 1;
-    test = test + [1, 1, 1];
-    writeln(test);
+    vector2df a;
+    dimension2df b;
+    a.X = 5;
+    b.Width = 10;
+
+    a = b;
+    a += b;
+    a = a * b;
+
+    writeln(a);
+
 }
