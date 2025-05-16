cdf8118e9239c9474828850232e996d4d2554240
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-16 19:44:24 -0400

    format

--- source/include/line3d.d
+++ source/include/line3d.d
@@ -21,40 +21,39 @@ import std.traits;
 
 //! 3D line between two points with intersection methods.
 // template <class T>
-struct line3d(T)
-{
+struct line3d(T) {
     // Basic any typecheck.
     static assert(isNumeric!T);
 
-	//! Start point of line
-	vector3d!T start = vector3d!T(0,0,0);
-	//! End point of line
-	vector3d!T end = vector3d!T(1,1,1);
+    //! Start point of line
+    vector3d!T start = vector3d!T(0, 0, 0);
+    //! End point of line
+    vector3d!T end = vector3d!T(1, 1, 1);
 
-	
+    //! Constructor with two points
+    this(T xa, T ya, T za, T xb, T yb, T zb) {
+        start = vector3d!T(xa, ya, za);
+        end = vector3d!T(xb, yb, zb);
+    }
+    //! Constructor with two points as vectors
+    this(const ref vector3d!T start, const ref vector3d!T end) {
+        this.start = start;
+        this.end = end;
+    }
 
-	//! Constructor with two points
-	this(T xa, T ya, T za, T xb, T yb, T zb) {
-			start = vector3d!T(xa, ya, za); end = vector3d!T (xb, yb, zb) ;
-            }
-	//! Constructor with two points as vectors
-	this(const ref vector3d!T start, const ref vector3d!T end) {
-			this.start = start; 
-            this.end = end;
-            }
-
-	// operators
+    // operators
 
     bool opEquals(const line3d!T other) const {
-        return (start == other.start && end == other.end) || (end == other.start && start == other.end);
+        return (start == other.start && end == other.end) || (end == other.start && start == other
+                .end);
     }
 
     // Negate.
     line3d!T opUnary(string s : "-")() const {
-        return line3d!T(-Start, - End);
+        return line3d!T(-Start, -End);
     }
 
-        // Assignment.
+    // Assignment.
     void opAssign(U)(U value) {
         // This is (half) compiler code. 
         // Give line3d even more assignments than C++.
@@ -76,15 +75,15 @@ struct line3d(T)
         }
     }
 
-        // Operator assignment.
+    // Operator assignment.
     ref line3d!T opOpAssign(string op, U)(const U value) {
         // This is compiler code. 
         // Give line3d even more assignment operator operators than C++.
         static if (__traits(isSame, U, line3d)) {
             mixin("start " ~ op ~ "= value.start;");
             mixin("end " ~ op ~ "= value.end;");
-            } else static if (isInstanceOf!(vector3d, U)) {
-                mixin("start " ~ op ~ "= value;");
+        } else static if (isInstanceOf!(vector3d, U)) {
+            mixin("start " ~ op ~ "= value;");
             mixin("end " ~ op ~ "= value;");
         } else static if (isArray!U) {
             static assert(isNumeric!(typeof(value[0])));
@@ -106,8 +105,8 @@ struct line3d(T)
         // Give line3d even more operators than C++.
         static if (__traits(isSame, U, line3d)) {
             mixin("return line3d!T(start " ~ op ~ " value.start, end " ~ op ~ " value.end);");
-            } else static if (isInstanceOf!(vector3d, U)) {
-                mixin("return line3d!T(start " ~ op ~ " value, end " ~ op ~ " value);");
+        } else static if (isInstanceOf!(vector3d, U)) {
+            mixin("return line3d!T(start " ~ op ~ " value, end " ~ op ~ " value);");
         } else static if (isArray!U) {
             static assert(isNumeric!(typeof(value[0])));
             assert(value.length == 3, "Cannot add array. Length is not 3.");
@@ -120,107 +119,101 @@ struct line3d(T)
         }
     }
 
+    // functions
+    //! Set this line to a new line going through the two points.
+    void setLine(const T xa, const T ya, const T za, const T xb, const T yb, const T zb) {
+        start.set(xa, ya, za);
+        end.set(xb, yb, zb);
+    }
+    //! Set this line to a new line going through the two points.
+    void setLine(const ref vector3d!T nstart, const ref vector3d!T nend) {
+        start.set(nstart);
+        end.set(nend);
+    }
+    //! Set this line to new line given as parameter.
+    void setLine(const ref line3d!T line) {
+        start.set(line.start);
+        end.set(line.end);
+    }
 
-	// functions
-	//! Set this line to a new line going through the two points.
-	void setLine(const T xa, const T ya, const T za, const T xb, const T yb, const T zb)
-	{
-		start.set(xa, ya, za);
-		end.set(xb, yb, zb);
-	}
-	//! Set this line to a new line going through the two points.
-	void setLine(const ref vector3d!T nstart, const ref vector3d!T nend)
-	{
-		start.set(nstart);
-		end.set(nend);
-	}
-	//! Set this line to new line given as parameter.
-	void setLine(const ref line3d!T line)
-	{
-		start.set(line.start);
-		end.set(line.end);
-	}
+    //! Get length of line
+    /** \return Length of line. */
+    T getLength() const {
+        return start.getDistanceFrom(end);
+    }
 
-	//! Get length of line
-	/** \return Length of line. */
-	T getLength() const { return start.getDistanceFrom(end); }
+    //! Get squared length of line
+    /** \return Squared length of line. */
+    T getLengthSQ() const {
+        return start.getDistanceFromSQ(end);
+    }
 
-	//! Get squared length of line
-	/** \return Squared length of line. */
-	T getLengthSQ() const { return start.getDistanceFromSQ(end); }
+    //! Get middle of line
+    /** \return Center of line. */
+    vector3d!T getMiddle() const {
+        return (start + end) / cast(T) 2;
+    }
 
-	//! Get middle of line
-	/** \return Center of line. */
-	vector3d!T getMiddle() const
-	{
-		return (start + end) / cast(T)2;
-	}
+    //! Get vector of line
+    /** \return vector of line. */
+    vector3d!T getVector() const {
+        return end - start;
+    }
 
-	//! Get vector of line
-	/** \return vector of line. */
-	vector3d!T getVector() const
-	{
-		return end - start;
-	}
-
-	//! Check if the given point is between start and end of the line.
-	/** Assumes that the point is already somewhere on the line.
+    //! Check if the given point is between start and end of the line.
+    /** Assumes that the point is already somewhere on the line.
 	\param point The point to test.
 	\return True if point is on the line between start and end, else false.
 	*/
-	bool isPointBetweenStartAndEnd(const ref vector3d!T point) const
-	{
-		return point.isBetweenPoints(start, end);
-	}
+    bool isPointBetweenStartAndEnd(const ref vector3d!T point) const {
+        return point.isBetweenPoints(start, end);
+    }
 
-	//! Get the closest point on this line to a point
-	/** \param point The point to compare to.
+    //! Get the closest point on this line to a point
+    /** \param point The point to compare to.
 	\return The nearest point which is part of the line. */
-	vector3d!T getClosestPoint(const ref vector3d!T point) const
-	{
-		vector3d!T c = point - start;
-		vector3d!T v = end - start;
-		T d = cast(T)v.getLength();
-		v /= d;
-		T t = v.dotProduct(c);
+    vector3d!T getClosestPoint(const ref vector3d!T point) const {
+        vector3d!T c = point - start;
+        vector3d!T v = end - start;
+        T d = cast(T) v.getLength();
+        v /= d;
+        T t = v.dotProduct(c);
 
-		if (t < cast(T)0.0)
-			return start;
-		if (t > d)
-			return end;
+        if (t < cast(T) 0.0)
+            return start;
+        if (t > d)
+            return end;
 
-		v *= t;
-		return start + v;
-	}
+        v *= t;
+        return start + v;
+    }
 
-	//! Check if the line intersects with a sphere
-	/** \param sorigin: Origin of the sphere.
+    //! Check if the line intersects with a sphere
+    /** \param sorigin: Origin of the sphere.
 	\param sradius: Radius of the sphere.
 	\param outdistance: The distance to the first intersection point.
 	\return True if there is an intersection.
 	If there is one, the distance to the first intersection point
 	is stored in outdistance. */
-	bool getIntersectionWithSphere(const ref vector3d!T sorigin, T sradius, f64 outdistance) const
-	{
-		const vector3d!T q = sorigin - start;
-		T c = q.getLength();
-		T v = q.dotProduct(getVector().normalize());
-		T d = sradius * sradius - (c * c - v * v);
+    bool getIntersectionWithSphere(const ref vector3d!T sorigin, T sradius, f64 outdistance) const {
+        const vector3d!T q = sorigin - start;
+        T c = q.getLength();
+        T v = q.dotProduct(getVector().normalize());
+        T d = sradius * sradius - (c * c - v * v);
 
-		if (d < 0.0)
-			return false;
+        if (d < 0.0)
+            return false;
 
-		outdistance = v - IrrMath.squareroot(d);
-		return true;
-	}
+        outdistance = v - IrrMath.squareroot(d);
+        return true;
+    }
 
-	
 };
 
 //! Typedef for an f32 line.
-alias line3df = line3d!f32 ;
+alias line3df = line3d!f32;
 //! Typedef for an integer line.
-alias line3di = line3d!s32 ;
+alias line3di = line3d!s32;
 
 // } // end namespace core
 // } // end namespace irr
